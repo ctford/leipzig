8ac7f89c1431e77e5ae09b120dc40aaee9ea0b55
Chris Ford <christophertford@gmail.com>
2012-01-04 18:52:51 +1100

    Double the bass and slow it down.

--- src/overtunes/songs/before.clj
+++ src/overtunes/songs/before.clj
@@ -22,7 +22,7 @@
   (chord :Eb4 :major)
   (chord :Eb4 :major)
   (chord :Bb3 :major)
-  (chord :Bb3 :major)
+  ()
 ])
 
 (def variation-section [
@@ -41,26 +41,27 @@
   (chord :Eb4 :major)
   (chord :Eb4 :major)
   (chord :Bb3 :major)
-  (chord :Bb3 :major)
+  ()
 ])
 
-
 (defn play-chord [notes duration] ( do 
-  (defn play-note [note] (organ-cornet note (/ duration (* 1000 0.9))))
-  (let [
-    root (first notes)
-    bass (- root 12)
-    with-bass (cons bass notes)
-  ]
-    (mix (map play-note (map midi->hz with-bass)))
+  (defn play-note [note] (organ-cornet note (/ duration 1000)))
+  (if (not (empty? notes))
+    (let [
+      root (first notes)
+      bass [(- root 12) (- root 24)]
+      with-bass (concat bass notes)
+    ]
+      (mix (map play-note (map midi->hz with-bass)))
+    )
   )
 ))
 
-(def metro (metronome 250))
+(def metro (metronome 90))
 (defn beat-length [metro] (- (metro 1) (metro 0))) 
 
 (defn play-progression [progression metro start] ( do
-  (if (> (count progression) 0) (do
+  (if (not (empty? progression)) (do
     (at (metro (+ start 0)) (play-chord (nth progression 0) (* 4 (beat-length metro))))
     (play-progression (rest progression) metro (+ start 4))
   ))
@@ -69,6 +70,8 @@
 (defn play [] ( 
   (play-progression
     (concat
+      starting-section
+      middle-section
       starting-section
       middle-section
       variation-section
