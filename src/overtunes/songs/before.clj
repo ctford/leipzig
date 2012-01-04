b39ed8a51b6ca9883c58a607e51ef683056c50eb
Chris Ford <christophertford@gmail.com>
2012-01-04 18:21:18 +1100

    Add a bass note beneath the chords.

--- src/overtunes/songs/before.clj
+++ src/overtunes/songs/before.clj
@@ -3,6 +3,17 @@
   (:use [overtunes.instruments.organ-cornet])
 )
 
+(def starting-section [
+  (chord :Eb3 :major)
+  (chord :Eb3 :major)
+  (chord :C3  :minor7)
+  (chord :C3  :minor7)
+  (chord :Eb3 :major)
+  (chord :Eb3 :major)
+  (chord :C3  :minor7)
+  (chord :C3  :minor7)
+])
+
 (def middle-section [
   (chord :G3  :minor)
   (chord :G3  :minor)
@@ -12,17 +23,6 @@
   (chord :Eb4 :major)
   (chord :Bb3 :major)
   (chord :Bb3 :major)
-])
-
-(def starting-section [
-  (chord :Eb3 :major)
-  (chord :Eb3 :major)
-  (chord :C3  :minor)
-  (chord :C3  :minor)
-  (chord :Eb3 :major)
-  (chord :Eb3 :major)
-  (chord :C3  :minor)
-  (chord :C3  :minor)
 ])
 
 (def variation-section [
@@ -44,14 +44,19 @@
   (chord :Bb3 :major)
 ])
 
+
 (defn play-chord [notes duration] ( do 
-  (defn play-note [note] (organ-cornet note (/ duration 1000)))
-  (mix 
-    (map play-note (map midi->hz notes))
+  (defn play-note [note] (organ-cornet note (/ duration (* 1000 0.9))))
+  (let [
+    root (first notes)
+    bass (- root 12)
+    with-bass (cons bass notes)
+  ]
+    (mix (map play-note (map midi->hz with-bass)))
   )
 ))
 
-(def metro (metronome 200))
+(def metro (metronome 250))
 (defn beat-length [metro] (- (metro 1) (metro 0))) 
 
 (defn play-progression [progression metro start] ( do
@@ -62,10 +67,13 @@
 ))
 
 (defn play [] ( 
-  (play-progression (concat
-    starting-section
-    middle-section
-    variation-section
+  (play-progression
+    (concat
+      starting-section
+      middle-section
+      variation-section
+    )
+    metro
+    (metro)
   )
-  metro (metro))
 ))
