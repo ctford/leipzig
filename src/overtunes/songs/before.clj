4f72fb6717bb0043a5811012714a87c4e8c19612
Chris Ford <christophertford@gmail.com>
2012-01-10 16:54:56 +1100

    Introduced relative metronomes.

--- src/overtunes/songs/before.clj
+++ src/overtunes/songs/before.clj
@@ -77,24 +77,31 @@
           with-bass (concat bass tones)]
       (doall (map (fn [tone] (play-note tone instrument duration)) with-bass)))))
 
-(defn play-progression [progression metro start]
+(defn metronome-from
+  "Returns a metronome that measures beats relative to start."
+  ([metro start] (fn
+    ([] (metro (+ 0 start)))
+    ([beat] (metro (+ beat start))))))
+
+(defn play-progression [progression metro]
   "Plays a seq of chords for two beats each on the cornet.
-  Takes a metronome and a starting beat in addition to the chord progression.
-  (play-progression [root fourth fifth] metro (metro))"
+  Takes a relative metronome in addition to the chord progression.
+  (play-progression [root fourth fifth] metro)"
   (let [beats-per-chord 2
         duration (* beats-per-chord (beat-length metro))]
     (when-not (empty? progression)
-      (at (metro start) (play-chord (first progression) organ-cornet duration))
-      (play-progression (rest progression) metro (+ start beats-per-chord)))))
+      (at (metro) (play-chord (first progression) organ-cornet duration))
+      (play-progression (rest progression)
+        (metronome-from metro beats-per-chord)))))
 
-(defn play-melody [melody metro start]
+(defn play-melody [melody metro]
   "Plays a seq of notes on the cornet.
-  Takes a metronome and a starting beat in addition to the melody.
-  (play-progression [root fourth fifth] metro (metro))"
+  Takes a relative metronome in addition to the melody.
+  (play-melody [:C3 :E3 :G3] metro)"
   (let [duration (beat-length metro)]
     (when-not (empty? melody)
-      (at (metro start) (play-note (first melody) organ-cornet duration))
-      (play-melody (rest melody) metro (+ start 1)))))
+      (at (metro) (play-note (first melody) organ-cornet duration))
+      (play-melody (rest melody) (metronome-from metro 1)))))
 
 (defn cycle-n
   "Returns a new seq which is cycled n times.
@@ -103,12 +110,12 @@
   (take (* (count s) n) (cycle s)))
 
 (defn play
-  "Play the melody over the chords to metro's time."
+  "Play the melody over the chords to the relative metro's time."
   [chords metro]
   (let [repetitions-per-chord (/ (count melody) 2) 
         melody-line (cycle-n (/ (count chords) repetitions-per-chord) melody)] 
-  (play-melody melody-line metro (metro))
-  (play-progression (concat chords finish) metro (metro))))
+  (play-melody melody-line metro)
+  (play-progression (concat chords finish) metro)))
 
 (defn full-version []
   "The full version of 'Before', grave."
