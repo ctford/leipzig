958b084cda4f468459ede4b51a6599cf7382ab16
Chris Ford <christophertford@gmail.com>
2012-01-10 23:46:18 +1100

    Use capitalised letters to make the music read more like the lyrics.

--- src/overtunes/songs/frere_jacques.clj
+++ src/overtunes/songs/frere_jacques.clj
@@ -3,18 +3,18 @@
   (:use [overtunes.core])
   (:use [overtunes.instruments.foo]))
 
-(def frère-jacques [:C4 :D4 :E4 :C4])
-(def dormez-vous [:E4 :F4 :G4 :rest])
-(def sonnez-les-matines [:G4 :G4 :E4 :C4])
-(def din-dan-don [:C4 :G3 :C4 :rest])
+(def Frère-Jacques [:C4 :D4 :E4 :C4])
+(def Dormez-vous? [:E4 :F4 :G4 :rest])
+(def Sonnez-les-matines! [:G4 :G4 :E4 :C4])
+(def Din-dan-don [:C4 :G3 :C4 :rest])
 
 (def melody (concat
-  frère-jacques frère-jacques
-  dormez-vous dormez-vous
-  sonnez-les-matines sonnez-les-matines
-  din-dan-don din-dan-don))
+  Frère-Jacques Frère-Jacques
+  Dormez-vous? Dormez-vous?
+  Sonnez-les-matines! Sonnez-les-matines!
+  Din-dan-don Din-dan-don))
 
-(defn frere-jacques-round []
+(defn frere-jacques []
   (let [metro (metronome 120)] 
     (play-melody melody foo metro)
     (play-melody melody foo (metronome-from metro 8))
