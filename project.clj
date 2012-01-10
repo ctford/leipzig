cf31cbad3ba6f751d9512898efb94d04212c8f5d
Chris Ford <christophertford@gmail.com>
2012-01-10 18:08:46 +1100

    Added the beat monad version of tarai mawashi (https://gist.github.com/1441831).

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Experiments with Overtone."
   :dependencies	[
     [org.clojure/clojure "1.3.0"]
+    [org.clojure/algo.monads "0.1.0"]
     [overtone "0.6.0"]
   ]
   :main overtunes.core
