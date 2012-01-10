067ef8981c53aa3506c2f69713be1c123fea97be
Chris Ford <christophertford@gmail.com>
2012-01-10 20:58:40 +1100

    No need to write out the lyrics because we have them embedded in clojure itself (including the accent).

--- src/overtunes/songs/frere_jacques.clj
+++ src/overtunes/songs/frere_jacques.clj
@@ -3,18 +3,13 @@
   (:use [overtunes.core])
   (:use [overtunes.instruments.foo]))
 
-;; Frère Jacques, frère Jacques,
-;; Dormez-vous? Dormez-vous?
-;; Sonnez les matines! Sonnez les matines!
-;; Din, dan, don. Din, dan, don.
-
-(def frere-jacques [:C4 :D4 :E4 :C4])
+(def frère-jacques [:C4 :D4 :E4 :C4])
 (def dormez-vous [:E4 :F4 :G4 :rest])
 (def sonnez-les-matines [:G4 :G4 :E4 :C4])
 (def din-dan-don [:C4 :G3 :C4 :rest])
 
 (def melody (concat
-  frere-jacques frere-jacques
+  frère-jacques frère-jacques
   dormez-vous dormez-vous
   sonnez-les-matines sonnez-les-matines
   din-dan-don din-dan-don
