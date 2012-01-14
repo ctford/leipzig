db6261a9f76773df381786c5adbf41de4906e6d2
Chris Ford <christophertford@gmail.com>
2012-01-14 22:38:07 +1100

    Added viola break and put bass under the whole thing.

--- src/overtunes/songs/sidhe.clj
+++ src/overtunes/songs/sidhe.clj
@@ -4,6 +4,7 @@
   (:use [overtunes.instruments.organ-cornet]))
 
 (defn zip [progression basses] (map cons basses progression))
+(defn with-bass [chord] (cons (- (first chord) 12) chord))
 
 (def descending-C-minor-scale [:C3 :Bb2 :Ab2 :G2 :F2 :Eb2 :D2 :C2])
 
@@ -19,8 +20,7 @@
   
    descending-C-minor-scale))
 
-
-(def At-last-he-reached
+(def Viola-instrumental-break (map with-bass
   [(chord :C4 :minor)
    (chord :G3 :minor)
    (chord :F3 :minor)
@@ -28,15 +28,17 @@
    (chord :Bb3 :major)
    (chord :A3 :major)
    (chord :D4 :minor)
-   (chord :D4 :minor)
-   (chord :Bb3 :major)
+   (chord :D4 :minor)]))
+
+(def At-last-he-reached (map with-bass
+  [(chord :Bb3 :major)
    (chord :A3 :major)
    (chord :D4 :minor)
    (chord :D4 :minor)
    (chord :Bb3 :major)
    (chord :A3 :major)
    (chord :D3 :minor)
-   (chord :D3 :minor)])
+   (chord :D3 :minor)]))
 
 (defn sidhe [] (let
   [m (metronome 120)
@@ -46,4 +48,5 @@
      metro))]
   (play-bars A-young-man-once (metronome-from m (* 0 4)))
   (play-bars A-young-man-once (metronome-from m (* 8 4)))
-  (play-bars At-last-he-reached (metronome-from m (* 16 4)))))
+  (play-bars Viola-instrumental-break (metronome-from m (* 16 4)))
+  (play-bars At-last-he-reached (metronome-from m (* 24 4)))))
