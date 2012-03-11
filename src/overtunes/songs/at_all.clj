e61a2aa75bbcbcff3f1c881aabe9668b4068d9b7
Chris Ford <christophertford@gmail.com>
2012-03-11 15:48:15 +0000

    Play all the opening melody (sorta).

--- src/overtunes/songs/at_all.clj
+++ src/overtunes/songs/at_all.clj
@@ -54,9 +54,12 @@
       (even-melody# (from timing 1) notes))))
 
 (defn play []
-  (let [timing (from (bpm 120) 4)]
-    (even-melody# timing (take 64 (cycle [9 7])))
+  (let [timing (from (bpm 120) 2)]
+    (even-melody# timing (take 32 (cycle [9 7])))
     (rythm-n-bass# timing (take 16 (cycle progression)))
-    (even-melody# (speed-up (from timing 63) 2) [4 7 9 7 7 4 7])))
+    (even-melody# (speed-up (from timing 31) 2) [4 7 9 7 7 4 7])
+    (even-melody# (speed-up (from timing 39) 2) [7 2 4 2 2 7 2])
+    (even-melody# (speed-up (from timing 47) 2) [7 2 4 2 2 7 2 4 5 7])
+    (even-melody# (speed-up (from timing 55) 2) [7 2 4 2 2 7 2 4 2 11])))
 
 (play)
