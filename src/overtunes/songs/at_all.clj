36749e6cb865dad3d9d07aceb63fa10571af1ce1
Chris Ford <christophertford@gmail.com>
2012-03-25 16:44:55 +0100

    Moved the scale into the ground function. De-generalises and simplifies a bunch of other stuff - tradeoff.

--- src/overtunes/songs/at_all.clj
+++ src/overtunes/songs/at_all.clj
@@ -8,34 +8,30 @@
         ms-per-beat (/ ms-per-minute beats-per-minute)]
     #(+ start (* ms-per-beat %))))
 
-(defn after [timing offset] #(timing (+ offset %)))
-(defn tempo [timing factor] #(timing (/ % factor)))
-(defn ground [note] (+ 60 note))
-
-(def note# (comp sampled-piano ground))
-(defn chord# [chord] (doseq [note (vals chord)] (note# note))) 
 
 (def ionian #(let [interval (mod % 7)
                   note ([0 2 4 5 7 9 11] interval)
                   octave (quot (- % interval) 7)]
                (+ (* 12 octave) note)))
 
-(defn triad [scale root]
-  (zipmap [:i :iii :v]
-          [(scale root)
-           (scale (+ root 2))
-           (scale (+ root 4))])) 
+(defn after [timing offset] #(timing (+ offset %)))
+(defn tempo [timing factor] #(timing (/ % factor)))
+(defn ground [note] (+ 60 (ionian note)))
 
-(defn lower [note] (- note 12))
-(defn raise [note] (+ note 12))
+(def note# (comp sampled-piano ground))
+(defn chord# [chord] (doseq [note (vals chord)] (note# note))) 
+
+(defn triad [root] (zipmap [:i :iii :v] [root (+ root 2) (+ root 4)]))
+(defn lower [note] (- note 7))
+(defn raise [note] (+ note 7))
 
 (defn with-base [chord]
   (assoc chord :base
          (lower (:i chord))))
 
-(def I (with-base (triad ionian 0)))
-(def II (with-base (triad ionian 1)))
-(def V (with-base (triad ionian 4)))
+(def I (with-base (triad 0)))
+(def II (with-base (triad 1)))
+(def V (with-base (triad 4)))
 
 (def progression [I I II II II V I (update-in V [:base] lower)])
 
@@ -60,40 +56,40 @@
         next))))
 
 (defn intro# [timing] 
-    (even-melody# timing (take 32 (cycle (map ionian [5 4]))))
+    (even-melody# timing (take 32 (cycle [5 4])))
     (rhythm-n-bass# timing (take 8 (cycle progression))))
 
 (defn first-bit# [timing]
   (-> timing
     (after -1)
     (tempo 2)
-    (even-melody# (map ionian [2 4 5 4 4 2 4]))
+    (even-melody# [2 4 5 4 4 2 4])
     (after 9)
-    (even-melody# (map ionian [-2 1 2 1 1 -2 1]))
+    (even-melody# [-2 1 2 1 1 -2 1])
     (after 9)
-    (even-melody# (map ionian [-2 1 2 1 1 -2 1 2 3 4]))
+    (even-melody# [-2 1 2 1 1 -2 1 2 3 4])
     (after 6)
-    (even-melody# (map ionian [-1 -2 -3 0 0 -3 0 1 0 -3])))
+    (even-melody# [-1 -2 -3 0 0 -3 0 1 0 -3]))
   (rhythm-n-bass# timing (take 8 (cycle progression))))
 
 (defn variation# [timing]
   (-> timing
     (tempo 2)
     (after 9)
-    (even-melody# (map ionian [11 11 12 9 7]))
+    (even-melody# [11 11 12 9 7])
     (after 11)
-    (even-melody# (map ionian [8 8 9 8 3]))
+    (even-melody# [8 8 9 8 3])
     (after 11)
-    (even-melody# (map ionian [8 8 9 6 4]))
+    (even-melody# [8 8 9 6 4])
     (after 11)
-    (even-melody# (map ionian [11 11 12 11 8])))
+    (even-melody# [11 11 12 11 8]))
   (first-bit# timing))
 
 (defn final-chord# [timing]
   (-> timing
     (after -1)
     (tempo 2)
-    (even-melody# (map ionian [11 13 14])))
+    (even-melody# [11 13 14]))
   (at (timing 0)
       (chord# (update-in I [:i] raise))))
 
