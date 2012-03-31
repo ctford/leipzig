5a807389e7a5796bc0911908b07e28eff0db068b
Chris Ford <christophertford@gmail.com>
2012-03-31 17:42:00 +0100

    Plays melody with rhythm, and as a round.

--- src/overtunes/songs/row_row_row_your_boat.clj
+++ src/overtunes/songs/row_row_row_your_boat.clj
@@ -3,30 +3,29 @@
     [overtone.live :only [at now]]
     [overtone.inst.sampled-piano :only [sampled-piano]]))
 
-(def major-scale #(apply + (take % (cycle [2 2 1 2 2 2 1]))))
+(defn sum-up-to [series n] (apply + (take n series)))
+(def major-scale #(sum-up-to (cycle [2 2 1 2 2 2 1]) %))
 (def g-major #(+ 67 (major-scale %))) 
 
 (defn ms-per-beat [ms start] #(+ start (* % ms)))
 (defn after [timing beats] #(timing (+ beats %)))
+(defn syncopate [timing lengths] #(timing (sum-up-to lengths %)))
 
-(def row-row-row-your-boat [0 0 0 1 2])
-(def gently-down-the-stream [2 1 2 3 4])
-(def merrily-merrily-merrily-merrily [7 7 7 4 4 4 2 2 2 0 0 0])
-(def life-is-but-a-dream [4 3 2 1 0])
-
-(def melody
-  (concat
-    row-row-row-your-boat
-    gently-down-the-stream
-    merrily-merrily-merrily-merrily
-    life-is-but-a-dream))
+(def pitches [0 0 0 1 2, 2 1 2 3 4, 7 7 7 4 4 4 2 2 2 0 0 0, 4 3 2 1 0])
+(def durations [1 1 2/3 1/3 1, 2/3 1/3 2/3 1/3 2, 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3, 2/3 1/3 2/3 1/3 2])
 
 (defn melody# [timing notes] 
   (let [note# #(at (timing %1) (sampled-piano (g-major %2)))]
     (dorun (map-indexed note# notes)))) 
 
 (defn play# []
-  (let [timing (ms-per-beat 700 (now))]
-    (melody# timing melody)))
+  (let [timing (ms-per-beat 500 (now))
+        rhythm #(syncopate % durations)]
+    (melody# (rhythm (after timing 0)) pitches)
+    (melody# (rhythm (after timing 16)) pitches)
+    (melody# (rhythm (after timing 20)) pitches)
+    (melody# (rhythm (after timing 24)) pitches)
+    (melody# (rhythm (after timing 28)) pitches)
+    ))
 
 (play#)
