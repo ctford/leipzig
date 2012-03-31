dcaff6eb232f75165e4c4d7ee346ca7d3e8cd900
Chris Ford <christophertford@gmail.com>
2012-03-31 19:13:09 +0100

    Extracted from fn.

--- src/overtunes/songs/row_row_row_your_boat.clj
+++ src/overtunes/songs/row_row_row_your_boat.clj
@@ -7,9 +7,10 @@
 (def major-scale (partial sum-n (cycle [2 2 1 2 2 2 1])))
 (def g-major #(-> % major-scale (+ 67))) 
 
-(defn bpm [beats start] #(-> % (/ beats) (* 60) (* 1000) (+ start)))
+(defn bpm [per-minute] #(-> % (/ per-minute) (* 60) (* 1000)))
 (defn after [timing beats] #(-> % (+ beats) timing))
 (defn syncopate [timing durations] #(-> (sum-n durations %) timing))
+(defn from [timing start] #(-> % timing (+ start))) 
 
 (def pitches [0 0 0 1 2, 2 1 2 3 4, 7 7 7 4 4 4 2 2 2 0 0 0, 4 3 2 1 0])
 (def durations [1 1 2/3 1/3 1, 2/3 1/3 2/3 1/3 2, 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3, 2/3 1/3 2/3 1/3 2])
@@ -19,7 +20,7 @@
     (dorun (map-indexed note# notes)))) 
 
 (defn play# []
-  (let [timing (bpm 150 (now))
+  (let [timing (-> (bpm 150) (from (now)))
         rhythm-from #(syncopate (after timing %) durations)]
     (melody# (rhythm-from 0)  pitches)
     (melody# (rhythm-from 16) pitches)
