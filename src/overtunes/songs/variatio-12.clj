97dab958b91607444f8088085a195bf6b852b980
Chris Ford <christophertford@gmail.com>
2012-04-09 16:32:41 +0100

    First three bars, written directly with translate. Has two missing 'accidentals' from the switch to D-major.

--- src/overtunes/songs/variatio-12.clj
+++ src/overtunes/songs/variatio-12.clj
@@ -14,22 +14,19 @@
 
 (defn bpm [per-minute] #(-> % (/ per-minute) (* 60) (* 1000)))
 (defn translate [f x y] #(-> % (+ x) f (+ y)))
-(def from #(translate %1 0 %2))
-(def after #(translate %1 %2 0))
 (defn syncopate [timing durations] #(->> % (sum-n durations) timing))
 
-
-(def pitches (flatten [-7 0 (range -1 4) (range 2 -1 -1)]))
-(def durations (flatten [1/2 (repeat 2 1/4) 1/2 (repeat 6 1/4)])) 
+(def pitches (flatten [-7 0 (range -1 4) (range 2 -1 -1) 4 (range 1 9) (range 7 -2 -1) 0]))
+(def durations (flatten [1/2 (repeat 2 1/4) 1/2 (repeat 6 1/4) (repeat 8 1/4) 3/2 (repeat 10 1/4)])) 
 
 (defn melody# [timing notes] 
   (let [note# #(at (timing %1) (piano (g-major %2)))]
     (dorun (map-indexed note# notes)))) 
 
 (defn play# []
-  (let [timing (-> (bpm 120) (from (now)))
-        rhythm-from #(syncopate (after timing %) durations)]
-    (melody# (rhythm-from 0)  pitches)
+  (let [timing (-> (bpm 120) (translate 0 (now)))
+        rhythm-from #(syncopate (translate timing % 0) durations)]
+    (melody# (rhythm-from 0) pitches)
     ))
 
 (play#)
