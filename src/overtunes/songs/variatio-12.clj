18310b13da987effc818865f20054fb7a6971113
Chris Ford <christophertford@gmail.com>
2012-04-09 15:43:30 +0100

    First bar of Varatio 12 - Canone alla Quarta.

---
+++ src/overtunes/songs/variatio-12.clj
@@ -0,0 +1,33 @@
+(ns overtunes.songs.variatio-12
+  (:use
+    [overtone.live :only [at now]]
+    [overtone.inst.sampled-piano :only [sampled-piano] :rename {sampled-piano piano}]))
+
+(defn sum-n [series n] (reduce + (take n series)))
+(defn scale [intervals]
+  #(if (neg? %)
+     (- ((scale (reverse intervals)) (- %)))
+     (sum-n (cycle intervals) %)))
+
+(def major (scale [2 2 1 2 2 2 1]))
+(def g-major #(-> % major (+ 67))) 
+
+(defn bpm [per-minute] #(-> % (/ per-minute) (* 60) (* 1000)))
+(defn after [timing beats] #(-> % (+ beats) timing))
+(defn syncopate [timing durations] #(->> % (sum-n durations) timing))
+(defn from [timing start] #(-> % timing (+ start))) 
+
+(def pitches (flatten [-7 0 (range -1 4) (range 2 -1 -1)]))
+(def durations (flatten [1/2 (repeat 2 1/4) 1/2 (repeat 6 1/4)])) 
+
+(defn melody# [timing notes] 
+  (let [note# #(at (timing %1) (piano (g-major %2)))]
+    (dorun (map-indexed note# notes)))) 
+
+(defn play# []
+  (let [timing (-> (bpm 120) (from (now)))
+        rhythm-from #(syncopate (after timing %) durations)]
+    (melody# (rhythm-from 0)  pitches)
+    ))
+
+(play#)
