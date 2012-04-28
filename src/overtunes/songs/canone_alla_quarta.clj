8eaf277fc982fa005fa28beb1ccd744b501806e4
Chris Ford <christophertford@gmail.com>
2012-04-28 23:46:59 +0100

    Rebuilding using a :time and :pitch keyed map so that the transformations can be more composable.

--- src/overtunes/songs/canone_alla_quarta.clj
+++ src/overtunes/songs/canone_alla_quarta.clj
@@ -1,12 +1,29 @@
 (ns overtunes.songs.canone-alla-quarta
   (:use
     [overtone.live :only [at now]]
-    [overtone.inst.sampled-piano :only [sampled-piano] :rename {sampled-piano piano}]))
+    [overtone.inst.sampled-piano :only [sampled-piano] :rename {sampled-piano piano#}]))
+
+(defn update-all [m [& ks] f]
+    (if ks
+          (update-in
+                  (update-all m (rest ks) f)
+                  [(first ks)]
+                  f)
+          m))
+
+(defn natural-map [ys] (zipmap (-> ys count range) ys))
+(defn natural-seq [f]
+    (if-let [y (f 0)]
+          (cons y (natural-seq (comp f inc)))
+          '()))
+
+(defn connect [f1 f2] #(if-let [y2 (f2 %)] (f1 y2) nil))
 
 (defn sum-n [series n] (reduce + (take n series)))
+(defn sums [series] (cons 0 (reductions + series)))
 (defn scale [intervals]
   #(if (neg? %)
-     (let [downward-scale (comp - (scale (reverse intervals)))]
+     (let [downward-scale (connect - (scale (reverse intervals)))]
        (-> % - downward-scale))
      (sum-n (cycle intervals) %)))
 
@@ -21,51 +38,38 @@
     (range a (inc b))
     (reverse (run b a))))
 
-(def durations (concat
-                 (repeat 2 1/4) [1/2]
-                 (repeat 14 1/4) [3/2]
-                 (repeat 10 1/4) [1/2]
-                 (repeat 2 1/4) [9/4 3/4]
-                 (repeat 12 1/4) [1/2 1 1/2]
-                 (repeat 12 1/4) [1])) 
+(def call {:time (concat (repeat 2 1/4) [1/2] (repeat 14 1/4) [3/2])
+           :pitch (concat [0] (run -1 3) (run 2 0) [4] (run 1 8))})
 
-(defn update-all [m [& ks] f]
-  (if ks
-    (update-in
-      (update-all m (rest ks) f)
-      [(first ks)]
-      f)
-    m)) 
-(defn sharps [notes] #(update-all (vec %) notes inc)) 
-(defn flats [notes] #(update-all (vec %) notes dec)) 
+(def response {:time (concat (repeat 10 1/4) [1/2] (repeat 3 1/4))
+               :pitch (concat (run 7 -1) [0] (run 0 -3))})
 
-(def leader-accidentals (comp (sharps [12 22]) (flats [37])))
-(def pitches (concat
-               [0] (run -1 3) (run 2 0)
-               [4] (run 1 8) (run 7 -1)
-               [0] (run 0 -3) [4 4] (run 2 -3)
-               [-1 1] (run 4 6)
-               [1 1 1 2 -1 -2 0 1 -1 -2] (run 5 0)))
+(defn subsequent [melody1 melody2]
+  (merge-with concat melody1 melody2))
 
-(def lower #(- % 7))
-(def bass-accidentals (sharps [8]))
-(def bass (map lower
-            (flatten
-              (map #(repeat 3 %) (concat (run 0 -3) (run -5 -3) [-7])))))
+(def melody
+  (let [functionalise #(update-all % [:time :pitch] natural-map)
+        ground-time #(update-in % [:time] sums)]
+    (functionalise (ground-time (subsequent call response)))))
 
-(defn melody# [timing notes] 
-  (let [note# #(at (timing %1) (piano %2))]
-    (dorun (map-indexed note# notes)))) 
+(defn melody# [melody] 
+  (let [notes (update-all melody [:pitch :time] natural-seq)
+        play-at #(at %1 (piano# %2))]
+    (dorun (map play-at (:time notes) (:pitch notes)))))
+
+(defn update [k f] #(update-in % [k] f))
+(def mirror (update :pitch #(connect % -))) 
+(defn echo [beats] (update :time #(translate % 0 beats)))
+(defn transpose [interval] (update :pitch #(translate % interval 0))) 
+(def canone-alla-quarta (reduce connect [(echo 3) (transpose -3) mirror])) 
 
 (defn play# []
-  (let [timing (-> (bpm 90) (translate 0 (now)))
-        rhythm-from #(syncopate (translate timing % 0) durations)
-        leader pitches
-        follower (->> leader (map -) (map #(- % 3)))]
-    (melody# timing (bass-accidentals (map g-major bass)))
-    (melody# (rhythm-from 1/2) (leader-accidentals (map g-major leader)))
-    (melody# (rhythm-from 7/2) (map g-major follower))
-    (melody# (syncopate (translate timing 23 0) [1 1/4 1/4 1/4 1/4 1]) (map g-major [-7 -7 -5 -3 -1 0]))
-    ))
+  (let [from-now #(translate % 0 (now))
+        beat (from-now (bpm 120))
+        with-beat #(update-in % [:time] (partial connect beat))
+        in-key #(update-in % [:pitch] (partial connect g-major))
+        leader (-> melody with-beat in-key)]
+  ;(-> leader canone-alla-quarta melody#)))
+  (-> leader melody#)))
 
 (play#)
