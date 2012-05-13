09a390b1e3ccd17a38a5d307a1254fa1a205e1a4
Chris Ford <christophertford@gmail.com>
2012-05-13 15:14:23 +0100

    Removed non-Goldberg songs and instruments.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject before "0.1.0-SNAPSHOT"
-  :description "Experiments with Overtone."
+  :description "The Goldberg Variations in Overtone."
   :dependencies	[
     [org.clojure/clojure "1.3.0"]
-    [org.clojure/algo.monads "0.1.0"]
     [overtone "0.6.0"]
   ]
 )
