f03d911b98706c7203a47b87c654f0caacc2e534
Chris Ford <christophertford@gmail.com>
2012-05-20 15:33:03 +0100

    Pulled some stuff out of the canone-alla-quarta# function so as to not complicate the finale.

--- src/goldberg/variations/canone_alla_quarta.clj
+++ src/goldberg/variations/canone_alla_quarta.clj
@@ -38,14 +38,14 @@
 
 (def major (scale [2 2 1 2 2 2 1]))
 (def minor (scale [2 1 2 2 1 2 2]))
+
 (def g-major (comp (partial + 67) major)) 
+(def d-major (comp (partial + 74) major)) 
 (def g-minor (comp (partial + 67) minor)) 
+(def d-minor (comp (partial + 74) minor)) 
 
-;(even-melody# (let [key (comp (partial + 55) minor), rest -100]
-;         (map key [0 1 2 0 0 1 2 0 2 3 4 rest 2 3 4 rest])))
-
-
-
+;(even-melody# (let [key (comp (partial + 55) major), rest -100]
+;                (map key [0 1 2 0 0 1 2 0 2 3 4 rest 2 3 4 rest])))
 
 
 
@@ -90,17 +90,17 @@
         development
           [(repeats [[1 3/4] [12 1/4] [1 1/2] [1 1] [1 1/2] [12 1/4] [1 3]])
           (runs [[4] [4] [2 -3] [-1 -2] [0] [3 5] [1] [1] [1 2] [-1 1 -1] [5 0]])]
-        [durations pitches] (map concat call response development)]
-    (map vector (accumulate durations) pitches)))
+        [durations pitches] (map concat call response development)
+        times (map (partial + 1/2) (accumulate durations))]
+    (map vector times pitches)))
 
 (def bass
   (let [triples (partial mapcat #(repeat 3 %))]
     (map vector
        (accumulate (repeats [[21 1] [13 1/4]]))
-       (concat (triples (runs [[0 -3] [-5 -3]])) (run [12 0])))))
+       (concat (triples (runs [[-7 -10] [-12 -10]])) (run [5 -7])))))
 
 ;melody
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Canone alla quarta - Johann Sebastian Bach   ;;
@@ -113,18 +113,18 @@
 
 (def mirror-canon (skew pitch -))
 (def crab-canon (skew timing -))
+(defn sloth-canon [factor] (skew timing (partial * factor)))
 (defn interval-canon [interval] (shift [0 interval]))
 (defn simple-canon [after] (shift [after 0]))
 (def table-canon (comp mirror-canon crab-canon))
 
 (defn canone-alla-quarta# [start tempo scale]
-  (let [in-time #(=> % (skew timing tempo) (shift [start 0]))
+  (let [in-time (comp (shift [start 0]) (skew timing tempo))
         in-key (skew pitch scale)
-        play-now# #(=> % in-key in-time play#)]
+        play-now# (comp play# in-key in-time)]
 
-    (=> bass (shift [0 -7]) play-now#)
-    (=> melody (shift [1/2 0]) play-now#)
-    (=> melody (simple-canon 7/2) mirror-canon (interval-canon -3) play-now#)))
+    (=> bass play-now#)
+    (=> melody play-now#)
+    (=> melody (simple-canon 3) mirror-canon (interval-canon -3) play-now#)))
 
 (canone-alla-quarta# (now) (bpm 90) g-major)
-;(stop)
