020a83dec5ed46bcc1ebd35f15b99df5ccc82aff
Chris Ford <christophertford@gmail.com>
2012-06-16 18:24:41 +0100

    Fade in the backing.

--- src/whelmed/instrument.clj
+++ src/whelmed/instrument.clj
@@ -3,14 +3,14 @@
     [overtone.live]))
 
 (definst shudder# [freq 440]
-  (let [envelope (env-gen (perc 0.1 5) :action FREE)]
+  (let [envelope (env-gen (perc 2 1.5) :action FREE)]
     (*
       (* envelope (sin-osc 6))
       (square freq)
       (sin-osc freq))))
 
-(definst sawish# [freq 440 depth 3]
-  (let [envelope (env-gen (perc 0.05 1.5) :action FREE)]
+(definst sawish# [freq 440]
+  (let [envelope (env-gen (perc 0.1 1.5) :action FREE)]
     (*
       envelope
-      (saw (+ freq (* depth (lf-saw:kr 0.1 0.2)))))))
+      (saw freq))))
