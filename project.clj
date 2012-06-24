0cea20171e8dad602789f3d272b8aec50b068bb1
Chris Ford <christophertford@gmail.com>
2012-06-24 16:37:52 +0100

    No need for aliases as we only have one track so far.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject whelmed "0.1.0-SNAPSHOT"
   :description "Whelmed. Not overwhelmed. Not underwhelmed. Just whelmed."
   :main whelmed.core
-  :run-aliases {:west whelmed.core/play#}
   :dependencies	[
     [org.clojure/clojure "1.3.0"]
     [overtone "0.7.0-SNAPSHOT"]
