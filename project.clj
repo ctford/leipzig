1bc696dbe89b629afb8762eb4ada08ded61e2f32
Sam Aaron <samaaron@gmail.com>
2012-07-07 01:54:13 +0100

    move to latest stable overtone and turn off aot compilation (works with lein2)

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject whelmed "0.1.0-SNAPSHOT"
   :description "Whelmed. Not overwhelmed. Not underwhelmed. Just whelmed."
-  :main whelmed.core
+  :main ^{:skip-aot true} whelmed.core
   :dependencies	[
     [org.clojure/clojure "1.3.0"]
-    [overtone "0.7.0-SNAPSHOT"]
-  ]
-)
+    [overtone "0.7.1"]])
