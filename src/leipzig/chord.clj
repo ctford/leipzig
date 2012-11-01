740cdbc3429c7e7e369d3df009b4967a92fb3bf5
Chris Ford <christophertford@gmail.com>
2012-11-01 21:59:38 +0530

    Added docstrings for chords.

--- src/leipzig/chord.clj
+++ src/leipzig/chord.clj
@@ -1,5 +1,16 @@
 (ns leipzig.chord)
 
-(defn triad [root] {:i root, :iii (+ root 2), :v (+ root 4)})
-(defn seventh [root] (-> root triad (assoc :vii (+ root 6))))
-(defn ninth [root] (-> root seventh (assoc :ix (+ root 8))))
+(defn triad
+  "Returns a triad from the specified root.
+  e.g. (triad 0)" 
+  [root] {:i root, :iii (+ root 2), :v (+ root 4)})
+
+(defn seventh
+  "Returns a seventh chord from the specified root.
+  e.g. (seventh 4)" 
+  [root] (-> root triad (assoc :vii (+ root 6))))
+
+(defn ninth
+  "Returns a ninth chord from the specified root.
+  e.g. (ninth 5)" 
+  [root] (-> root seventh (assoc :ix (+ root 8))))
