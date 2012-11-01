e23ba2dd0da1ab335dfa448ab191ced01752de0a
Chris Ford <christophertford@gmail.com>
2012-11-01 17:16:25 +0530

    Fixed up docstrings.

--- src/leipzig/canon.clj
+++ src/leipzig/canon.clj
@@ -2,28 +2,28 @@
   (:use [leipzig.melody]))
 
 (defn canon
-  "Accompany notes with a melody created by apply f to notes.
+  "Accompanies notes with a melody created by applying f to notes.
   e.g. (->> leader (canon (simple 4)))"
   [f notes] (with notes (f notes)))
 
 (defn- from [base] (partial + base))
 
 (defn simple
-  "Returns a transformation of delaying a melody by wait."
+  "Returns a transformation that delays a melody by wait."
   [wait] (partial where :time (from wait)))
 
 (defn interval
-  "Returns a transformation of raising a melody by interval."
+  "Returns a transformation that raises a melody by interval."
   [interval] (partial where :pitch (from interval)))
 
 (def mirror
-  "Returns a transformation of reflecting a melody by pitch."
+  "Returns a transformation that reflects a melody over pitch."
   (fn [notes] (->> notes (where :pitch -))))
 
 (def crab
-  "Returns a transformation of reflecting a melody by time."
+  "Returns a transformation that reflects a melody over time."
   (fn [notes] (->> notes (where :time -))))
 
 (def table
-  "Returns a transformation reflecting a melody by time and pitch."
+  "Returns a transformation that reflects a melody over time and pitch."
   (comp mirror crab))
