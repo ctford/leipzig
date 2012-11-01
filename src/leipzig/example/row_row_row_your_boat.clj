7957091b9b57cf35fe3ef305775a2f2d5d32cfa5
Chris Ford <christophertford@gmail.com>
2012-11-01 23:42:29 +0530

    Added bass to example.

--- src/leipzig/example/row_row_row_your_boat.clj
+++ src/leipzig/example/row_row_row_your_boat.clj
@@ -5,10 +5,12 @@
     leipzig.canon
     overtone.inst.sampled-piano))
 
-(defmethod play-note :default
+(defmethod play-note :leader
   [{midi :pitch}] (sampled-piano midi))
 (defmethod play-note :follower
   [{midi :pitch}] (sampled-piano (+ midi 12)))
+(defmethod play-note :bass
+  [{midi :pitch}] (sampled-piano (- midi 12)))
 
 (def melody
   (->> (phrase [3/3 3/3 2/3 1/3 3/3]
@@ -21,10 +23,18 @@
                (mapcat (partial repeat 3) [7 4 2 0])))
     (then
        (phrase [2/3 1/3 2/3 1/3 6/3] 
-               [  4   3   2   1   0]))))
+               [  4   3   2   1   0]))
+    (where :part (is :leader))))
+
+(def bass
+  (->> (phrase [1  1 2]
+               [0 -3 0])
+     (where :part (is :bass))
+     (times 4)))
 
 (defn row-row [speed key]
   (->> melody
+    (with bass)
     (times 2)
     (canon (comp (simple 4)
                  (partial where :part (is :follower))))
