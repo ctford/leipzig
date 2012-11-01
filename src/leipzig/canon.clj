c17c5387f07bf5d8c916917314e56006a8de6bf6
Chris Ford <christophertford@gmail.com>
2012-11-01 17:02:57 +0530

    Added docstrings for canon.

--- src/leipzig/canon.clj
+++ src/leipzig/canon.clj
@@ -1,12 +1,29 @@
 (ns leipzig.canon
   (:use [leipzig.melody]))
 
-(defn canon [f notes] (concat notes (f notes)))
+(defn canon
+  "Accompany notes with a melody created by apply f to notes.
+  e.g. (->> leader (canon (simple 4)))"
+  [f notes] (with notes (f notes)))
 
 (defn- from [base] (partial + base))
 
-(defn simple [wait] (partial where :time (from wait)))
-(defn interval [interval] (partial where :pitch (from interval)))
-(def mirror (fn [notes] (->> notes (where :pitch -))))
-(def crab (fn [notes] (->> notes (where :time -))))
-(def table (comp mirror crab))
+(defn simple
+  "Returns a transformation of delaying a melody by wait."
+  [wait] (partial where :time (from wait)))
+
+(defn interval
+  "Returns a transformation of raising a melody by interval."
+  [interval] (partial where :pitch (from interval)))
+
+(def mirror
+  "Returns a transformation of reflecting a melody by pitch."
+  (fn [notes] (->> notes (where :pitch -))))
+
+(def crab
+  "Returns a transformation of reflecting a melody by time."
+  (fn [notes] (->> notes (where :time -))))
+
+(def table
+  "Returns a transformation reflecting a melody by time and pitch."
+  (comp mirror crab))
