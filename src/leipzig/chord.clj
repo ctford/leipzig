2ae55befb2d4e31a9f22951e83c0dc4161bcf07b
Chris Ford <christophertford@gmail.com>
2012-11-01 20:54:32 +0530

    Chords needs to be functions because you usually want to say you've got a seventh at the fourth etc.

--- src/leipzig/chord.clj
+++ src/leipzig/chord.clj
@@ -1,17 +1,5 @@
 (ns leipzig.chord)
 
-(def neutral-scale
-  (zipmap
-    [:i :ii :iii :iv :v :vi :vii :viii :ix :xii :xiii]
-    (range)))
-
-(defn- add [chord k] (-> note (assoc k (neutral-scale k))))
-(defn diminish [chord k] (-> note (update-in [k] #(- % 1/2))))
-(defn augment [chord k] (-> note (update-in [k] #(+ % 1/2))))
-
-(def triad (-> neutral-scale (select-keys [:i :iii :v])))
-(def sixth (-> triad (add :vi)))
-(def seventh (-> triad (add :vii)))
-(def ninth (-> seventh (add :ix)))
-(def eleventh (-> ninth (add :xi)))
-(def thirteenth (-> eleventh (add :xiii)))
+(defn triad [root] {:i root, :iii (+ root 2), :v (+ root 4)})
+(defn seventh [root] (-> root triad (assoc :vii (+ root 6))))
+(defn ninth [root] (-> root seventh (assoc :ix (+ root 8))))
