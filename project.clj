0649b157307d643a7cf25f4de2454d1842799beb
Chris Ford <christophertford@gmail.com>
2012-11-21 21:26:19 +0530

    Exclude seesaw to get rid of warnings due to bad dependencies.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/ctford/leipzig"
   :dependencies	[
     [org.clojure/clojure "1.4.0"]
-    [overtone "0.7.1"]
+    [overtone "0.7.1" :exclusions [seesaw]]
     [midje "1.4.0"]
     [org.clojure/math.numeric-tower "0.0.1"]])
