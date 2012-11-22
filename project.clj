52aaeb4e1165b6516536f87797c5e5b3b3d81ce5
Chris Ford <christophertford@gmail.com>
2012-11-22 21:28:15 +0530

    Added license reference in project.clj so it will end up as metadata on clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject leipzig "0.2.0-SNAPSHOT"
   :description "A composition library for Overtone."
   :url "http://github.com/ctford/leipzig"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo
+            :comments "Same as Overtone"}
   :dependencies	[
     [org.clojure/clojure "1.4.0"]
     [overtone "0.7.1" :exclusions [seesaw]]
