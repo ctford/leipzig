75cf4bfe4b9b241341d493701e4e8464e6e4bb95
Chris Ford <christophertford@gmail.com>
2013-03-24 12:14:40 +0300

    Added inversions to chords.

--- src/leipzig/chord.clj
+++ src/leipzig/chord.clj
@@ -1,16 +1,18 @@
-(ns leipzig.chord)
+(ns leipzig.chord
+ (:require [clojure.set :as set]))
 
-(defn- mapval [m f]
-  ((fn update-all [m [k & ks] f]
-     (if k
-       (-> m (update-in [k] f) (update-all ks f))
-       m))
-     m (keys m) f))
+(defn- from [n] (partial + n))
+(defn- lower [chord n k] (update-in chord [k] (from (- n))))
+(defn- update-all [m [k & ks] f]
+ (if k
+   (-> m (update-in [k] f) (update-all ks f))
+   m))
+(defn- mapval [m f] (update-all m (keys m) f))
 
 (defn root
   "Translates a chord so that its root is at tonic.
   e.g. (-> triad (root 4))" 
-  [chord tonic] (-> chord (mapval #(+ % tonic)))) 
+  [chord tonic] (-> chord (mapval (from tonic)))) 
 
 (def triad
   "A three-tone chord."
@@ -23,3 +25,10 @@
 (def ninth 
   "A five-tone chord."
   (-> seventh (assoc :ix 8)))
+
+(defn inversion [chord n]
+  "Drops all but the first n tones of the chord.
+  e.g. (-> triad (inversion 2))"
+  (let [stable (->> [:i :iii :v] (take n) set) 
+        lowered (set/difference (-> chord keys set) stable)]
+    (update-all chord (seq lowered) (from -7))))
--- test/leipzig/test/chord.clj
+++ test/leipzig/test/chord.clj
@@ -12,3 +12,15 @@
 (fact
   (-> ninth (root 5)) =>
   {:i 5, :iii 7, :v 9, :vii 11, :ix 13})
+
+(fact "The first inversion drops everything except the tonic."
+  (-> triad (inversion 1)) =>
+  {:i 0, :iii -5, :v -3})
+
+(fact "The second inversion drops everything except the tonic and the third."
+  (-> seventh (inversion 2)) =>
+  {:i 0, :iii 2, :v -3, :vii -1})
+
+(fact "The third inversion drops everything except the tonic, third and fifth."
+  (-> seventh (inversion 3)) =>
+  {:i 0, :iii 2, :v 4, :vii -1})
