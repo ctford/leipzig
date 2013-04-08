602f73e20e0cb4f43107a8d6473be8eda8d0a464
Chris Ford <christophertford@gmail.com>
2013-04-08 13:46:08 +0300

    Improved some test docstrings.

--- test/leipzig/test/temperament.clj
+++ test/leipzig/test/temperament.clj
@@ -5,32 +5,31 @@
   (temperament/equal 69)       => 440.0
   (temperament/pythagorean 69) => 440)
 
-(fact "Raising an octave doubles the frequency."
-  (temperament/equal 69)        => (roughly 440)
+(fact "An equal temperament octave (roughly) doubles the frequency."
   (temperament/equal (+ 69 12)) => (roughly 880) 
   (temperament/equal (- 69 12)) => (roughly 220))
 
-(fact "A perfect fifth is flat by about two cents."
+(fact "A equal temperament perfect fifth is flat by about two cents."
   (let [cent (java.lang.Math/pow 2 1/1200)]
     (/ (* 3/2 (temperament/equal 69)) (temperament/equal (+ 69 7)))
       => (roughly (java.lang.Math/pow cent 1.96))
     (/ (temperament/equal (- 69 7)) (* 2/3 (temperament/equal 69)))
       => (roughly (java.lang.Math/pow cent 1.96))))
 
-(fact "A major third is sharp by about 14 cents."
+(fact "An equal temperament major third is sharp by about 14 cents."
   (let [cent (java.lang.Math/pow 2 1/1200)]
     (/ (temperament/equal (+ 69 4)) (* 5/4 (temperament/equal 69)))
       => (roughly (java.lang.Math/pow cent 13.69))
     (/ (* 4/5  (temperament/equal 69)) (temperament/equal (- 69 4)))
       => (roughly (java.lang.Math/pow cent 13.69))))
 
-(fact "A semitone is the twelfth root of two."
+(fact "An equal temperament semitone is the twelfth root of two."
   (/ (temperament/equal 70) (temperament/equal 69)) =>
     (roughly (java.lang.Math/pow 2 1/12))
   (/ (temperament/equal 69) (temperament/equal 68)) =>
     (roughly (java.lang.Math/pow 2 1/12)))
 
-(fact "Just temperament has ideal fifths, fourths and octaves."
+(fact "Pythagorean temperament has ideal fifths, fourths and octaves."
   (/ (temperament/pythagorean (+ 69 6))  (temperament/pythagorean 69)) => 3/2
   (/ (temperament/pythagorean (- 69 7))  (temperament/pythagorean 69)) => 2/3
   (/ (temperament/pythagorean (+ 69 4))  (temperament/pythagorean 69)) => 4/3 
