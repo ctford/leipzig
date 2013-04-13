5549b589a166a5c6398b054f85588b8f7f2a0044
Chris Ford <christophertford@gmail.com>
2013-04-13 07:29:27 +0300

    Added the jam fn.

--- src/leipzig/live.clj
+++ src/leipzig/live.clj
@@ -25,3 +25,27 @@
       trickle
       (map (fn [{epoch :time :as note}] (->> note play-note (overtone/at epoch))))
       dorun)))
+
+(defn- from [base] (partial + base))
+
+(defn- forever [riff]
+  (let [{final :time, duration :duration} (last @riff)]
+    (concat
+      @riff
+      (lazy-seq (->> (forever riff) (melody/where :time (from (+ final duration))))))))
+
+(defn jam*
+  "Plays riff repeatedly, freshly dereferencing it each time.
+  riff must be a var, not an arbitrary expression.
+  e.g. (jam* (var riff))"
+  [riff]
+  (->> riff forever play))
+
+(defmacro jam 
+  "Plays riff repeatedly, freshly dereferencing it each time.
+  riff must be a symbol, not an arbitrary expression.
+  e.g.
+    (def riff (phrase [1 2] [3 4])) 
+    (jam riff)"
+  [riff] 
+  `(jam* (var ~riff))) 
