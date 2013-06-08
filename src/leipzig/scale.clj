87d6a5e8dc93aae4408d6e659340b58d0d63a87f
Chris Ford <christophertford@gmail.com>
2013-06-08 19:43:17 +0300

    Beef up scale docstrings.

--- src/leipzig/scale.clj
+++ src/leipzig/scale.clj
@@ -1,9 +1,9 @@
 (ns leipzig.scale
   (:require [clojure.math.numeric-tower :as math]))
 
-(defmacro defs ^:private [names values]
+(defmacro defs [names docstring values]
   `(do ~@(map
-     (fn [name value] `(def ~name ~value))
+     (fn [name value] `(def ~name ~docstring ~value))
      names (eval values))))
 
 (defn- sum-n [series n] (apply + (take n series)))
@@ -27,26 +27,33 @@
         fraction (- degree (math/floor degree))]
   (+ lower (* fraction (- upper lower)))))
 
-(def major (scale [2 2 1 2 2 2 1]))
-(def blues (scale [3 2 1 1 3 2]))
-(def pentatonic (scale [3 2 2 3 2]))
-(def chromatic (scale [1]))
+(def major "Seven-tone scale, commonly used in Western music." (scale [2 2 1 2 2 2 1]))
+(def blues "Six-tone scale, used for blues music." (scale [3 2 1 1 3 2]))
+(def pentatonic "Five-tone scale, common to East Asian music."(scale [3 2 2 3 2]))
+(def chromatic "Scale consisting of all twelve tones." (scale [1]))
 
 (defn from [base] (partial + base))
 
-(defs [sharp flat] [inc dec])
-(defs [C D E F G A B]
+(defs
+  [C D E F G A B]
+  "A key, expressed as a translation function."
   (map
     (comp from (from 60) major)
     (range)))
+
+(defs
+  [sharp flat]
+  "A modification of a key, expressed as a translation function."
+  [inc dec])
 
 (defn mode [scale n] (comp #(- % (scale n)) scale (from n)))
 
 (defs
   [ionian dorian phrygian lydian mixolydian aeolian locrian]
+  "A heptatonic mode."
   (map (partial mode major) (range)))
 
-(def minor aeolian)
+(def minor "Natural minor is another name for the Aeolian mode." aeolian)
 
 (defn low
   "Lower midi one octave.
