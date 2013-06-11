4e67bda01e9cd117e69637f1ea9151b7efaa93cb
Chris Ford <christophertford@gmail.com>
2013-06-11 11:38:51 +0300

    Beef up some canon tests.

--- test/leipzig/test/canon.clj
+++ test/leipzig/test/canon.clj
@@ -10,7 +10,7 @@
   (->> [{:time 1 :pitch 1}] (canon (comp (simple 3) (interval 4)))) =>
     [{:time 1 :pitch 1} {:time 4 :pitch 5}])
 
-(fact "Crab canons reverse notes based on when they finish."
+(fact "Crab canons reflect notes based on when they finish."
   (->> (melody/rhythm [1/2 1]) (canon crab)) => 
     [{:duration 1/2 :time 0} {:duration 1 :time 0}
      {:duration 1 :time 1/2} {:duration 1/2 :time 1}])
@@ -22,3 +22,15 @@
 (fact "Canon is lazy."
   (->> (melody/rhythm (repeat 1)) (canon (simple 1/2)) (take 2)) =>
     [{:time 0 :duration 1} {:time 1/2 :duration 1}])
+
+(fact "Simple delays a melody"
+  ((simple 4) [{:time 0 :duration 1} {:time 1 :duration 2}]) =>
+    [{:time 4 :duration 1} {:time 5 :duration 2}])
+
+(fact "Mirror reflects a melody over pitch."
+  (mirror [{:time 0 :pitch 1} {:time 1 :pitch 2}]) =>
+    [{:time 0 :pitch -1} {:time 1 :pitch -2}])
+
+(fact "Interval shifts a melody over pitch."
+  ((interval 2) [{:time 0 :pitch 1} {:time 1 :pitch 2}]) =>
+    [{:time 0 :pitch 3} {:time 1 :pitch 4}])
