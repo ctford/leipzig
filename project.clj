98bad1a404a11533d51cede4941734294f8013ea
Chris Ford <christophertford@gmail.com>
2014-06-07 21:25:14 +0100

    Working towards 0.9.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leipzig "0.8.1"
+(defproject leipzig "0.9.0-SNAPSHOT"
   :description "A composition library for Overtone."
   :url "http://github.com/ctford/leipzig"
   :license {:name "The MIT License"
