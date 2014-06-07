426b39d1f29497d7918496b8bd7e830b2674965f
Chris Ford <christophertford@gmail.com>
2014-06-07 21:18:11 +0100

    Point release to fix the example that broke with the Overtone upgrade.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leipzig "0.8.0"
+(defproject leipzig "0.8.1"
   :description "A composition library for Overtone."
   :url "http://github.com/ctford/leipzig"
   :license {:name "The MIT License"
@@ -13,4 +13,4 @@
              {:plugins [[lein-midje "3.1.3"]
                         [codox "0.8.8"]]
               :dependencies [[midje "1.6.3"]]}}
-  :codox {:src-dir-uri "http://github.com/ctford/leipzig/blob/0.8.0/"})
+  :codox {:src-dir-uri "http://github.com/ctford/leipzig/blob/0.8.1/"})
