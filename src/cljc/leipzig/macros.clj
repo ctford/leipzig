cd52c7cfece528d466ffef977a1a4975f9eb4651
Chris Ford <christophertford@gmail.com>
2016-01-04 23:37:38 +1100

    More elegant defs solution - thanks to @gfredericks.

--- src/cljc/leipzig/macros.clj
+++ src/cljc/leipzig/macros.clj
@@ -1,7 +1,6 @@
 (ns leipzig.macros)
 
 (defmacro defs [names docstring values]
-  `(do ~@(map
-     (fn [name i] `(def ~name ~docstring (nth ~values ~i)))
-     names
-     (range))))
+  `(let [[~@names] ~values]
+     ~@(for [name names]
+       `(def ~name ~docstring ~name))))
