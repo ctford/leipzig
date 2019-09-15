1f062a8be0656982064e83f620a14e9fdf09239c
Chris Ford <christophertford@gmail.com>
2019-09-15 17:36:10 +0200

    Bump midje and lein-midje versions.

--- project.clj
+++ project.clj
@@ -11,12 +11,12 @@
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :source-paths ["src/clj" "src/cljc"]
   :profiles {:dev
-             {:plugins [[lein-midje "3.1.3"]
+             {:plugins [[lein-midje "3.2.1"]
                         [lein-cljsbuild "1.1.1"]
                         [codox "0.8.8"]]
               :dependencies  [[com.cemerick/piggieback "0.2.1"]
                               [org.clojure/tools.nrepl "0.2.10"]
-                              [midje "1.6.3"] ]
+                              [midje "1.9.9"] ]
               :repl-options  {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :cljsbuild {:builds {:prod {:source-paths ["src/cljc"]
                               :compiler     {:output-to     "target/cljs/leipzig.js"
