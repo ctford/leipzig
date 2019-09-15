b5e83ebffd32d96a99aeda38f924fd4a79905467
Chris Ford <christophertford@gmail.com>
2019-09-15 21:45:35 +0200

    Docstring for phrase tests.

--- test/leipzig/test/melody.clj
+++ test/leipzig/test/melody.clj
@@ -54,7 +54,7 @@
     [{:time 0 :duration 1 :part :drum}
      {:time 1 :duration 2 :part :drum}])
 
-(fact
+(fact "phrase builds a (potentially polyphonic) melody."
   (phrase [1 2] [3 4]) =>
     [{:time 0 :duration 1 :pitch 3}
      {:time 1 :duration 2 :pitch 4}]
